2fb94e7847543e9689474d343ed53de56506c9d1
weasley187 <54782676+weasley187@users.noreply.github.com>
2020-03-31 16:00:00 +0300

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,10 +1,9 @@
 # encode your payload: http://string-functions.com/base64encode.aspx
 # accrobata e gei
-
 import base64
 
 def exec_xor():
-	xor = 'yourcode'
+	xor = 'yourpayload'
 	decode = str(base64.b64decode(xor))
 	exec(decode)
 	#print('{}'.format(decode))
